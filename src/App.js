875111caa922875037e48ea9efc762da527c67e7
Larry Boyer <larryboyer@Larrys-MacBook-Pro.local>
2021-04-05 00:21:53 -0400

    Work on achieve, mm logos and lightbulb.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import logo from './logo.svg';
+import achieve_logo from './img/achieve_logo_02.png';
+import mm_bulb from './img/mm_bulb_00.png';
+import maker_madness from './img/maker_madness_00.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={achieve_logo} className="achieve-logo" alt="logo" />
+        <img src={mm_bulb} className="achieve-logo" alt="logo" />
+        <img src={maker_madness} className="achieve-logo" alt="logo" />
         <p>
           MakerMadnessWeb5
         </p>
         <p>
           What Will You Make Today?
         </p>
+		{/* Hey there 
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,6 +26,7 @@ function App() {
         >
           Learn React
         </a>
+		*/}
       </header>
     </div>
   );
