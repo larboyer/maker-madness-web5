340641b30236839720d040a687f98f59f76d4e61
Frank Stallone III <frank@stallone.dev>
2021-04-10 18:04:35 -0400

    refactor: ♻️  Moved the Maker logo to the header

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import mm_bulb from '../img/mm_bulb_00.png';
 import achieve_logo from '../img/achieve_logo_02.png';
-import maker_madness from '../img/maker_madness_00.png';
  
 const Home = () => {
 	return (
-		<div className="max-w-xl sm:mx-auto my-8 mx-8">
-			<img src={maker_madness} className="achieve-logo" alt="logo" /><br />
+		<div className="max-w-xl sm:mx-auto my-8 mx-8 text-center">
 			<img src={mm_bulb} className="achieve-logo" alt="logo" /><br />
 			<img src={achieve_logo} className="achieve-logo" alt="logo" />
 			<p>Home page body content</p>
