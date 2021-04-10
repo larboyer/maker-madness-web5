9ff340b0e91295b1bfe53fb69e0c100d68fa15d4
Frank Stallone III <Frank.Stallone@adp.com>
2021-04-10 14:23:06 -0400

    feat: ✨ Adding Tailwind w/ JIT compiler

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ import maker_madness from '../img/maker_madness_00.png';
  
 const Home = () => {
 	return (
-		<div>
+		<div className="max-w-xl sm:mx-auto my-8 mx-8">
 			<img src={maker_madness} className="achieve-logo" alt="logo" /><br />
 			<img src={mm_bulb} className="achieve-logo" alt="logo" /><br />
 			<img src={achieve_logo} className="achieve-logo" alt="logo" />
--- src/components/Navigation2.js
+++ src/components/Navigation2.js
@@ -1,29 +1,33 @@
 import React from 'react';
 import { Nav, NavItem } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLightbulb } from '@fortawesome/free-solid-svg-icons';
 
 const Navigation2 = (props) => {
 	return (
-		<nav className="navbar navbar-expand-md navbar-light sticky-top" 	role="navigation">
-			<div className="container-fluid">
-				<a className="navbar-brand" href="/">MM</a>
-				<Nav className="ml-auto">
-					<NavItem>
-						<NavLink to="/make" className="nav-link">
-							Make
-						</NavLink>
-					</NavItem>
-					<NavItem>
-						<NavLink to="/code" className="nav-link">
-							Code
-						</NavLink>
-					</NavItem>
-					<NavItem>
-						<NavLink to="/give" className="nav-link">
-							Give
-						</NavLink>
-					</NavItem>
-				</Nav>
+		<nav className="py-4 w-full sticky top-0 shadow-md bg-white" role="navigation">
+			<div className="mx-auto max-w-3xl">
+				<div className="grid grid-cols-2 mx-4">
+					<a href="/" className="logo text-yellow-700 hover:text-yellow-600 active:text-yellow-500"><FontAwesomeIcon className="fill-current" icon={faLightbulb}/></a>
+					<Nav className="flex flex-row space-x-2 place-self-end">
+						<NavItem>
+							<NavLink to="/make" className="nav-link">
+								Make
+							</NavLink>
+						</NavItem>
+						<NavItem>
+							<NavLink to="/code" className="nav-link">
+								Code
+							</NavLink>
+						</NavItem>
+						<NavItem>
+							<NavLink to="/give" className="nav-link">
+								Give
+							</NavLink>
+						</NavItem>
+					</Nav>
+				</div>
 			</div>
 		</nav>
 	)
