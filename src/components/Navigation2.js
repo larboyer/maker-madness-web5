30701f5073744e184dc5922fefe3a368415a3b5b
Frank Stallone III <Frank.Stallone@adp.com>
2021-04-10 14:34:22 -0400

    bug: 🐛 Node 14 required for JIT compiler

--- src/components/Navigation2.js
+++ src/components/Navigation2.js
@@ -8,8 +8,8 @@ const Navigation2 = (props) => {
 	return (
 		<nav className="py-4 w-full sticky top-0 shadow-md bg-white" role="navigation">
 			<div className="mx-auto max-w-3xl">
-				<div className="grid grid-cols-2 mx-4">
-					<a href="/" className="logo text-yellow-700 hover:text-yellow-600 active:text-yellow-500"><FontAwesomeIcon className="fill-current" icon={faLightbulb}/></a>
+				<div className="grid grid-cols-2 mx-4 sm:mx-0">
+					<a href="/" className="logo text-yellow-600 hover:text-yellow-500 active:text-yellow-500"><FontAwesomeIcon className="fill-current" icon={faLightbulb}/></a>
 					<Nav className="flex flex-row space-x-2 place-self-end">
 						<NavItem>
 							<NavLink to="/make" className="nav-link">
