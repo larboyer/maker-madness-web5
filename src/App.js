7e58f9c0e070c99ace582670e3398ee0fd7f7aa3
Frank Stallone III <frank@stallone.dev>
2021-04-12 19:42:35 -0400

    bug: 🐛 Fixed footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 // import achieve_logo from './img/achieve_logo_02.png';
 // import mm_bulb from './img/mm_bulb_00.png';
 // import maker_madness from './img/maker_madness_00.png';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import "./App.css";
-import Home from "./components/Home";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import './App.css';
+import Home from './components/Home';
 // import TakeNMake from './components/TakeNMake';
-import Make from "./components/Make";
-import Code from "./components/Code";
-import Give from "./components/Give";
+import Make from './components/Make';
+import Code from './components/Code';
+import Give from './components/Give';
 // import CodeJoy from './components/CodeJoy';
 // import Home2 from './components/Home2';
 // import About from './components/About';
 // import Contact from './components/Contact';
 // import Navigation from './components/Navigation';
-import Navigation2 from "./components/Navigation2";
-import Navigation3 from "./components/Navigation3";
-import TakeMake01 from "./components/TakeMake01";
-import TakeMake02 from "./components/TakeMake02";
-import TakeMake03 from "./components/TakeMake03";
+import Navigation2 from './components/Navigation2';
+import TakeMake01 from './components/TakeMake01';
+import TakeMake02 from './components/TakeMake02';
+import TakeMake03 from './components/TakeMake03';
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
@@ -34,7 +34,7 @@ class App extends Component {
           <Route path="/takemake02" component={TakeMake02} exact />
           <Route path="/takemake03" component={TakeMake03} exact />
         </Switch>
-        <Navigation3 />
+        <Footer />
       </BrowserRouter>
     );
   }
--- src/components/Navigation2.js
+++ src/components/Navigation2.js
@@ -28,8 +28,6 @@ const Navigation2 = (props) => {
     menuState ? null : 'hidden',
   ]);
 
-  console.log(menuState);
-
   return (
     <div className="relative bg-white border-b-2">
       <div className="max-w-7xl mx-auto px-4 sm:px-6">
