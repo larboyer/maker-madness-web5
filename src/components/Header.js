4276c0cf2dcc2cf93fe8d233f0c3151bdfe58225
Frank Stallone III <Frank.Stallone@adp.com>
2021-04-13 15:56:56 -0400

    refactor: ♻️  Moved mobile menu its own file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import classNames from 'classnames';
 import { NavLink } from 'react-router-dom';
+import MobileNavigation from './MobileNavigation';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faLightbulb,
@@ -40,11 +41,6 @@ const Header = (props) => {
       setMakeDropMenuState(true);
     }
   };
-
-  const mobileMenuClasses = classNames([
-    'absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden',
-    mobileMenuState ? 'z-10' : 'hidden',
-  ]);
 
   const makeDropDownClasses = classNames([
     'absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2',
@@ -188,119 +184,10 @@ const Header = (props) => {
           </div>
         </div>
       </div>
-      {/* Mobile Navigation */}
-      <div className={mobileMenuClasses}>
-        <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
-          <div className="pt-5 pb-6 px-5">
-            <div className="flex items-center justify-between">
-              <div>
-                <a
-                  href="/"
-                  className="logo text-yellow-600 hover:text-yellow-500 active:text-yellow-500 flex flex-row items-center space-x-2"
-                >
-                  <span className="sr-only">Maker Madness 2021</span>
-                  <FontAwesomeIcon
-                    className="fill-current"
-                    icon={faLightbulb}
-                  />
-                  <img src={maker_madness} className="h-6" alt="logo" />
-                </a>
-              </div>
-              <div className="-mr-2">
-                <button
-                  type="button"
-                  onClick={handleMobileMenu}
-                  className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
-                >
-                  <span className="sr-only">Close menu</span>
-                  {/* <!-- Heroicon name: outline/x --> */}
-                  <svg
-                    className="h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
-                </button>
-              </div>
-            </div>
-            <div className="mt-6">
-              <nav className="grid gap-y-8">
-                <NavLink
-                  to="/make"
-                  className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
-                >
-                  <FontAwesomeIcon
-                    className="flex-shrink-0 h-6 w-6 text-blue-600"
-                    icon={faTools}
-                  />
-                  <span className="ml-3 text-base font-medium text-gray-900">
-                    Make
-                  </span>
-                </NavLink>
-
-                <NavLink
-                  to="/make"
-                  className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
-                >
-                  <FontAwesomeIcon
-                    icon={faToolbox}
-                    className="flex-shrink-0 h-6 w-6 text-blue-600"
-                  />
-                  <span className="ml-3 text-base font-medium text-gray-900">
-                    Take&amp;Make
-                  </span>
-                </NavLink>
-
-                <NavLink
-                  to="#"
-                  className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
-                >
-                  {/* <!-- Heroicon name: outline/cursor-click --> */}
-                  <FontAwesomeIcon
-                    icon={faPaperPlane}
-                    className="flex-shrink-0 h-6 w-6 text-blue-600"
-                  />
-                  <span className="ml-3 text-base font-medium text-gray-900">
-                    OYO
-                  </span>
-                </NavLink>
-
-                <NavLink
-                  to="/code"
-                  className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
-                >
-                  <FontAwesomeIcon
-                    className="flex-shrink-0 h-6 w-6 text-blue-600"
-                    icon={faCode}
-                  />
-                  <span className="ml-3 text-base font-medium text-gray-900">
-                    Code
-                  </span>
-                </NavLink>
-              </nav>
-            </div>
-          </div>
-          <div className="py-6 px-5 space-y-6">
-            <div>
-              <NavLink
-                to="/give"
-                className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-              >
-                Give
-              </NavLink>
-            </div>
-          </div>
-        </div>
-      </div>
+      <MobileNavigation
+        show={mobileMenuState}
+        clickHandler={handleMobileMenu}
+      />
     </div>
   );
 };
