8feb7d43c1a3ee5d95a4aef65f88868860ba0ca3
Frank Stallone III <Frank.Stallone@adp.com>
2021-04-13 06:56:52 -0400

    feat: ✨ Adding general inner page scaffolding and roughing in content

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,141 @@
-import React from "react";
-import mm_bulb from "../img/mm_bulb_00.png";
-import achieve_logo from "../img/achieve_logo_02.png";
+import React from 'react';
+import mm_bulb from '../img/mm_bulb_00.png';
+import achieve_logo from '../img/achieve_logo_02.png';
 
 const Home = () => {
   return (
-    <div className="max-w-xl sm:mx-auto my-8 mx-8 text-center">
-      <img src={mm_bulb} alt="logo" />
-      <br />
-      <img src={achieve_logo} alt="logo" />
-      <p>Home page body content</p>
+    <div class="relative py-16 bg-white overflow-hidden">
+      <div class="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
+        <div
+          class="relative h-full text-lg max-w-prose mx-auto"
+          aria-hidden="true"
+        >
+          <svg
+            class="absolute top-12 left-full transform translate-x-32"
+            width="404"
+            height="384"
+            fill="none"
+            viewBox="0 0 404 384"
+          >
+            <defs>
+              <pattern
+                id="74b3fd99-0a6f-4271-bef2-e80eeafdf357"
+                x="0"
+                y="0"
+                width="20"
+                height="20"
+                patternUnits="userSpaceOnUse"
+              >
+                <rect
+                  x="0"
+                  y="0"
+                  width="4"
+                  height="4"
+                  class="text-gray-200"
+                  fill="currentColor"
+                />
+              </pattern>
+            </defs>
+            <rect
+              width="404"
+              height="384"
+              fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)"
+            />
+          </svg>
+          <svg
+            class="absolute top-1/2 right-full transform -translate-y-1/2 -translate-x-32"
+            width="404"
+            height="384"
+            fill="none"
+            viewBox="0 0 404 384"
+          >
+            <defs>
+              <pattern
+                id="f210dbf6-a58d-4871-961e-36d5016a0f49"
+                x="0"
+                y="0"
+                width="20"
+                height="20"
+                patternUnits="userSpaceOnUse"
+              >
+                <rect
+                  x="0"
+                  y="0"
+                  width="4"
+                  height="4"
+                  class="text-gray-200"
+                  fill="currentColor"
+                />
+              </pattern>
+            </defs>
+            <rect
+              width="404"
+              height="384"
+              fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)"
+            />
+          </svg>
+          <svg
+            class="absolute bottom-12 left-full transform translate-x-32"
+            width="404"
+            height="384"
+            fill="none"
+            viewBox="0 0 404 384"
+          >
+            <defs>
+              <pattern
+                id="d3eb07ae-5182-43e6-857d-35c643af9034"
+                x="0"
+                y="0"
+                width="20"
+                height="20"
+                patternUnits="userSpaceOnUse"
+              >
+                <rect
+                  x="0"
+                  y="0"
+                  width="4"
+                  height="4"
+                  class="text-gray-200"
+                  fill="currentColor"
+                />
+              </pattern>
+            </defs>
+            <rect
+              width="404"
+              height="384"
+              fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)"
+            />
+          </svg>
+        </div>
+      </div>
+      <div class="relative px-4 sm:px-6 lg:px-8">
+        <div class="text-lg max-w-prose mx-auto">
+          <h1>
+            <span class="block text-base text-center text-indigo-600 font-semibold tracking-wide uppercase">
+              Maker Madness
+            </span>
+            <span class="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+              Home
+            </span>
+          </h1>
+          <p class="mt-8 text-xl text-gray-500 leading-8">
+            Make awsome things. Do it often!
+          </p>
+        </div>
+        <div class="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
+          <img src={mm_bulb} alt="logo" />
+          <br />
+          <img src={achieve_logo} alt="logo" />
+          <p>
+            Faucibus commodo massa rhoncus, volutpat. <strong>Dignissim</strong>{' '}
+            sed <strong>eget risus enim</strong>. Mattis mauris semper sed amet
+            vitae sed turpis id. Id dolor praesent donec est. Odio penatibus
+            risus viverra tellus varius sit neque erat velit. Faucibus commodo
+            massa rhoncus, volutpat. Dignissim sed eget risus enim.{' '}
+            <a href="/">Mattis mauris semper</a> sed amet vitae sed turpis id.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
